75df05fd1d31b0800acd3d68eb9a341c753a65e3
Schevon <ojoseph445@gmail.com>
2015-07-27 21:30:06 -0400

    final commit before submit

--- readme.txt
+++ readme.txt
@@ -1,36 +1,16 @@
-Movie Trailer Website Version 1.0 6/29/15
-readme.txt
+Udacity Full Stack Web Developer Project 1 - Movie Trailer Website
 
-I. File list
---------------
- Video.py
- video_config.py
- fresh_tomatoes.py
-
-II.How To Run "movie trailer website":
----------------
-Navigate into VideoTrailerProject folder
-Using a Python IDE such as IDLE,open and run video_config.py
-After compilation a movie_trailer.html file will be generated within that very same folder. 
-Use any browser (preferably Chrome) to open and run  movie_trailer.html.
-Click on any of the box art images to play their corresponding trailer.
-
-III. Design and Implementation
---------------
- Movie descriptions,images and trailers are  courtesy of imdb.com,wikipedia.com,google.com and youtube.com  
+File list
+Video.py
+video_config.py
+fresh_tomatoes.py
  
- video.py  creates a movie class and initializes its constructor. For good OOP practice
- another .py file (video_config.py) contains instances of the movie object in which variables are passed into the movie constructor.
- These values passed into the various movie objects are previously stored in variables of type String just for code readability purposes.
- These movie objects are then stored in an array and then passed into a method of fresh_tomatoes.py called openUrl().
- fresh_tomatoes.py  code was given by Udacity staff and was modified  to meet the necessary specifications.
+This static web page displays a list of my favourite  "5 star movies" with their corresponding
+youtube trailers,box art imagery and descriptions. This project uses Python and HTML to aid in
+developing server-side code to store a list of movies within Python objects.
 
-IV.General Installation Notes
---------------
-No installation notes required.
 
-Contact:
-===========================
-name:   Schevon Joseph
-voice:  803-257-7436
-email:  chevtech123@gmail.com
+How to Run: 
+To run the script, clone this repository directory and run video_config.py. 
+If executed successfully, a new html page fresh_tomatoes will be generated or overwritten. 
+You can view the page by opening fresh_tomatoes.html with any web browser.
--- video_config.py
+++ video_config.py
@@ -1,63 +1,42 @@
 import Video
 import fresh_tomatoes
 
-# each movie image url saved in a variable of type String
-sin_image = '''http://ia.media-imdb.com/images/M/MV5BMTI2NjUyMDUyMV5BMl5BanBnXkFtZ
-TcwMzU0OTkyMQ@@._V1_SX640_SY720_.jpg'''
-warm_image = "http://ecx.images-amazon.com/images/I/51Mxun8Z0XL.jpg"
-carrie_image = '''http://wp.production.patheos.com/blogs/poptheology/files/2013/10/
-carrie-poster-2013.jpg'''
-obli_image = '''http://ia.media-imdb.com/images/M/MV5BMTQwMDY0MTA4MF5BMl5BanBnXk
-FtZTcwNzI3MDgxOQ@@._V1_SX214_AL_.jpg'''
-fight_image = "http://doublefeatureshow.com/images/covers/fight-club.jpg"
-taken_image = '''http://ia.media-imdb.com/images/M/MV5BNjM5MDU3NTY0M15BMl5BanBnXkFtZT
-gwOTk2ODU2MzE@._V1_SX640_SY720_.jpg'''
 
-# each movie youtube trailer url saved in a variable of type String
-sin_trailer = "https://www.youtube.com/watch?v=C3VjVMitTbA"
-warm_trailer = "https://www.youtube.com/watch?v=c9RQe5WBbww"
-carrie_trailer = "https://www.youtube.com/watch?v=LxwPUSWRaJo"
-obli_trailer = "https://www.youtube.com/watch?v=6ob3ygJ-zfY"
-fight_trailer = "https://www.youtube.com/watch?v=SUXWAEX2jlg"
-taken_trailer = "https://www.youtube.com/watch?v=uPJVJBm9TPA"
+#creating an instance of type movie for each movie
+#calling Movies init() and passing required arguments
 
-# each movie description saved in a variable of type String
-sin_desc = """A film that explores the dark and miserable town, Basin City,
-and tells the story of three different people,
-all caught up in violent corruption."""
+sin_city = Video.Movie("Sin City",
+                       "images/sin_city.jpg",
+                       "https://www.youtube.com/watch?v=C3VjVMitTbA",
+                       "A film that explores the dark and miserable town, Basin City.")
 
-warm_desc = """A terrible plague has left the planet's population divided
-between zombies and humans. An unusual zombie
-sees his walking-dead brethren attacking a living woman and rescues her."""
+warm_bodies = Video.Movie("Warm Bodies",
+                          "images/warm_bodies.jpg",
+                          "https://www.youtube.com/watch?v=c9RQe5WBbww",
+                          "A terrible plague has left the planet's population divided.")
 
-carrie_desc = """A reimagining of the classic horror tale about Carrie White, a
-shy girl outcast by her peers and sheltered by her
-deeply religious mother, who unleashes telekinetic terror on her small town."""
+carrie = Video.Movie("Carrie",
+                     "images/carrie.jpg",
+                     "https://www.youtube.com/watch?v=LxwPUSWRaJo",
+                     "Classic horror tale about Carrie White, a shy girl outcast by her peers.")
 
-obli_desc = """A veteran assigned to extract Earth's remaining resources
-begins to question what he knows about his mission and himself."""
+oblivion = Video.Movie("Oblivion",
+                       "images/oblivion.jpg",
+                       "https://www.youtube.com/watch?v=6ob3ygJ-zfY",
+                       "A veteran assigned to extract Earth's resources question his mission.")
 
-fight_desc = """An insomniac office worker, looking for a way to change his
-life, crosses paths with a devil-may-care soap maker,
-forming an underground fight club that evolves into
-something much, much more..."""
+fight_club = Video.Movie("Fight Club",
+                         "images/fight_club.jpg",
+                         "https://www.youtube.com/watch?v=SUXWAEX2jlg",
+                         "An insomniac office worker, looking for a way to change his life.")
 
-taken_desc = """Ex-covert operative Bryan Mills (Liam Neeson) and his ex-wife,
-Lenore (Famke Janssen), are enjoying a reconciliation
-when Lenore is brutally murdered. """
+taken = Video.Movie("Taken 3",
+                    "images/taken.jpg",
+                    "https://www.youtube.com/watch?v=uPJVJBm9TPA",
+                    "Ex-covert operative ex-wife is brutally murdered.")
 
-# creating an instance of type movie for each movie
-# calling Movies init() and passing required arguments
+#every movie object is stored in an array called movies
+movies=[sin_city,warm_bodies,carrie,oblivion,fight_club,taken]
 
-sin_city = Video.Movie("Sin City", sin_image, sin_trailer, sin_desc)
-warm_bodies = Video.Movie("Warm Bodies", warm_image, warm_trailer, warm_desc)
-carrie = Video.Movie("Carrie", carrie_image, carrie_trailer, carrie_desc)
-oblivion = Video.Movie("Oblivion", obli_image, obli_trailer, obli_desc)
-fight_club = Video.Movie("Fight Club", fight_image, fight_trailer, fight_desc)
-taken = Video.Movie("Taken 3", taken_image, taken_trailer, taken_desc)
-
-# every movie object is stored in an array called movies
-movies = [sin_city, warm_bodies, carrie, oblivion, fight_club, taken]
-
-# .open_movies_page() is called which takes an array of movie objects
+#.open_movies_page() is called which takes an array of movie objects
 fresh_tomatoes.open_movies_page(movies)
